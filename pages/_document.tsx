8d5a75e5b20cbdeaf6d7c7d5906875965eb3e270
Abengkris <abengkrischannel@gmail.com>
2024-11-18 00:38:39 +0700

    fix meta twitter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,8 @@ export default function Document() {
   const meta = {
     title: "Abengkris",
     description: "Author, bloger, and bitcoiner",
-    image: "https://image.nostr.build/bcfc506a9e376af381d48f7293ff415488b42eeab03a424fb7a73dd6c04a786d.jpg",
+    image:
+      "https://image.nostr.build/bcfc506a9e376af381d48f7293ff415488b42eeab03a424fb7a73dd6c04a786d.jpg",
   };
 
   return (
@@ -17,7 +18,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@abengkriss" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
